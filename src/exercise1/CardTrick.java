3a39c4b4bfc61d24228dc27ca654417224ecc9fb
Melannie Del Socorro <delsocor@sheridancollege.ca>
2023-05-30 14:04:20 -0400

    final commit

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -63,6 +63,8 @@ public class CardTrick {
      * replace this information with your own.
      * @author Paul Bonenfant Jan 2022
      */
+    
+        // I'm Done!
         private static void printInfo() {
     
         System.out.println("Congratulations, you guessed right!");
