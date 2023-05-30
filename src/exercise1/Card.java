30e553e73cb91578107a092b0a063474e5fdf44f
Melannie Del Socorro <delsocor@sheridancollege.ca>
2023-05-30 10:00:43 -0400

    2nd commit

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -1,28 +1,46 @@
 package exercise1;
 
-import java.util.Random;
-
+/**
+ * A class that models playing card Objects. Cards have 
+ * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
+ * A suit (clubs, hearts, spades, diamonds).
+ * There are 52 cards in a deck, no jokers.
+ * This code is to be used in ICE1. When you create your own branch,
+ * add your name as a modifier.
+ * @author dancye
+ * @author Paul Bonenfant May 2020
+ */
 public class Card {
 
-  // private String suit; //clubs, spades, diamonds, hearts
-   //private int value;//1-13
+   private String suit; //clubs, spades, diamonds, hearts
+   private int value;//1-13
 
-   private static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-   private static final String [] RANK ={"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};   
-   private static final int Num_Cards=7;
-   
-   private String[][] cards;
-   
-   public Card(){
-       generateCards();
-   }
-   
-public void generateCards(){
-        Random random = new Random();
-        cards=new String[Num_Cards][2];
-        for(int i=0; i<Num_Cards; i++){
-            
-        }
+   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }  
 }
-}
-   
